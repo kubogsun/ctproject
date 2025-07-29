b3042616658bce2a810e51dae1c3146dfb8e0d6c
kubogsun <equpequp@naver.com>
2025-07-29 13:23:20 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,26 +1,18 @@
 import streamlit as st
-import streamlit.components.v1 as htmlviewer
-
+import streamlit.componnets.v1 as htmlviewer
 # Title Msg#1
 st.title('This is Sky Webapp!!')
-
-with open('./index.html', 'r', encoding='utf-8') as f:
-    html = f.read()
-    f.close()
-
-# html = '''
-# <html>
-#     <head>
-#         <title> this is my html </title>
-#     </head>
-
-#     <body>
-#         <h1>Topic</h1>
-#         <h2>SubTopic</h2>
-#     </body>
-# </html>
-# '''
-
+html = '''
+<html>
+    <head>
+        <title> this is my html </title>
+    </head>
+    <body>
+        <h1>Topic</h1>
+        <h2>SubTopic</h2>
+    </body>
+</html>
+'''
 # Box#1(4), Box#2(1)
 col1, col2 = st.columns((4,1))
 with col1:
@@ -29,17 +21,15 @@ with col1:
         st.info('Content..')
         st.video(url)
 
+    with st.expander('Image content...'):
+        imgfilepath = './images/gpt.png'
+        st.image(imgfilepath)
+        
     with st.expander('Content #2...'):
         #st.write(html, unsafe_allow_html=True)
-        htmlviewer.html(html, height=600)
-
-    with st.expander('Content #3...'):
-        #st.write(html, unsafe_allow_html=True)
-        htmlviewer.html(html, height=600)
-
+        htmlviewer.html(html)
 with col2:
     with st.expander('Tips..'):
         st.info('Tips..')
-
 st.markdown('<hr>', unsafe_allow_html=True)
-st.write('<font color="BLUE">(c)copyright. all rights reserved by skykang', unsafe_allow_html=True) 
+st.write('<font color="BLUE">(c)copyright. all rights reserved by skykang', unsafe_allow_html=True)
